9585b85ea9209fb8c5f37ddbe3de0a9fa968b8ca
Athurg Feng <hi@atfeng.com>
2020-10-30 16:16:35 +0800

    添加默认日报Content生成的函数

--- topapi_report_create.go
+++ topapi_report_create.go
@@ -20,6 +20,27 @@ type ReportContentToSend struct {
 	ContentType string `json:"content_type"`
 }
 
+// GenerateDailyReport 生成日报的Content
+func GenerateDailyReport(today, todo, support string) []ReportContentToSend {
+	if today == "" {
+		today = "无"
+	}
+
+	if todo == "" {
+		todo = "无"
+	}
+
+	if support == "" {
+		support = "无"
+	}
+
+	return []ReportContentToSend{
+		ReportContentToSend{Key: "今日完成工作", Sort: 0, Type: 1, ContentType: "markdown", Content: today},
+		ReportContentToSend{Key: "未完成工作", Sort: 1, Type: 1, ContentType: "markdown", Content: todo},
+		ReportContentToSend{Key: "需协调工作", Sort: 2, Type: 1, ContentType: "markdown", Content: support},
+	}
+}
+
 //CreateReport 创建日志，返回日志ID
 func CreateReport(fromUserId, templateId string, contents []ReportContentToSend, toUserIds, toGroupIds []string) (string, error) {
 	param := url.Values{
